34121f8fa7118101107e856ded7f09cda0716522
Fernando Albuquerque <fer.albuquerque90@gmail.com>
2017-11-22 22:43:25 -0200

    realizado todos os desafios do documento Atividade_2__Xamarin_MVVM.pdf

--- Xamarin.EX2FIAP/Xamarin.EX2FIAP/App.xaml.cs
+++ Xamarin.EX2FIAP/Xamarin.EX2FIAP/App.xaml.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Xamarin.EX2FIAP.ViewModel;
 using Xamarin.Forms;
 
 namespace Xamarin.EX2FIAP
 {
     public partial class App : Application
     {
+        public static AlunoViewModel AlunoVM { get; set; }
+
         public App()
         {
             InitializeComponent();
+            InitializeApplication();
 
-            MainPage = new NavigationPage(new View.AlunoView());
+            MainPage = new NavigationPage(new View.AlunoView() { BindingContext = App.AlunoVM });
+        }
+
+        private void InitializeApplication()
+        {
+            if (AlunoVM == null) AlunoVM = new AlunoViewModel();
         }
 
         protected override void OnStart()
--- Xamarin.EX2FIAP/Xamarin.EX2FIAP/View/NovoAlunoView.xaml.cs
+++ Xamarin.EX2FIAP/Xamarin.EX2FIAP/View/NovoAlunoView.xaml.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.EX2FIAP.ViewModel;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Xamarin.EX2FIAP.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AlunoView : ContentPage
+    public partial class NovoAlunoView : ContentPage
     {
-
-        public AlunoView()
+        public NovoAlunoView()
         {
             InitializeComponent();
         }
